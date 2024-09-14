3815754e150697d0b7cae52aed1466bf3710d11f
Ammara Gul <139876497+Ammara-aziz@users.noreply.github.com>
2024-09-14 11:13:57 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+# print("Hello World")
+# print("I am in container")
+
+# control flow statement
+# # if statement
+
+# if Logic condition:
+if True:
+    print("Hello World")
+    print("I am in container")
+    
+if 2>5: # False
+    print("Mind your Paws")
+    # Functions Call
+    # Input
+    print("I am in container")    
+
+#Take Input
+# input("Enter any value")
+
+# my_name : str = input("Enter your Name ")
+#     return my_name
+
+def get_user_name(age:int, name:str) -> str:
+    my_name : str = input("Enter your Name ")
+    return my_name
+
+# Invoke function
+# new_user_name:str = get_user_name()
+
+# Functions
+def clasName():
+    # Function Body
+    
+    print("I am learning functions in Python")
+    
+# function Call (Invoke)
+clasName()    
+
+fruits : list = ['bnanana', 'apple', 'mango']
+
+
+for i in range (len(fruits)):
+    print(fruits[i])
+    
+print('----------\n')
+    
